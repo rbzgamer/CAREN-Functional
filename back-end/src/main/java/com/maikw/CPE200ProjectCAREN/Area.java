272f59fc62638bc90e6a1d7f4461e2b9ec9a1d77
Autoz <76035467+rbzgamer@users.noreply.github.com>
2023-03-25 01:28:40 +0700

    change back from for each to default method

--- back-end/src/main/java/com/maikw/CPE200ProjectCAREN/Area.java
+++ back-end/src/main/java/com/maikw/CPE200ProjectCAREN/Area.java
@@ -12,8 +12,8 @@ public class Area {
     protected double radius = 100;
     protected String name;
     protected List<Virus> rebornQueue;
-    private final boolean DEBUG = true;
 
+    // Constructor
     public Area(String name){
         this(name, new ArrayList<>());
     }
@@ -26,16 +26,20 @@ public class Area {
         this.name = name;
     }
 
+    // Impure function
+    // Impure function: Modifies state of units
     public void addUnit(Unit dummy){
         this.units.add(dummy);
     }
 
+    // Impure function: Modify state of viruses and units
     public void addVirus(Virus dummy){
         dummy.setArea(this);
         this.viruses.add(dummy);
         this.addUnit(dummy);
     }
 
+    // Impure function: Modify state of viruses
     /**
      *
      * @param dummy รับเป็น list ของ Virus เพื่อที่ง่ายต่อการใช้งานที่เก็บมา
@@ -43,40 +47,48 @@ public class Area {
     public void addAllVirus(List<Virus> dummy){
         for(Virus virus : dummy){
             this.addVirus(virus);
-            if(DEBUG) System.out.println("Set to area = " + virus.area.getName());
         }
     }
 
+    // Impure function: Modifies state of antibodies and units
     public void addAntibody(Antibody dummy){
         dummy.setArea(this);
         this.antibodies.add(dummy);
         this.addUnit(dummy);
     }
 
+    // Impure function: Modify state of antibodies
     /**
      *
      * @param dummy รับเป็น list ของ Antibody เพื่อที่ง่ายต่อการใช้งานที่เก็บมา
      */
     public void addAllAntibody(List<Antibody> dummy){
-        antibodies.forEach(this::addAntibody);
+        for(Antibody antibody : dummy){
+            this.addAntibody(antibody);
+        }
     }
 
+    // Impure function: Modifies state of units
     public void removeUnit(Unit dummy){
         this.units.remove(dummy);
+        dummy.setArea(null);
     }
 
+    // Impure function: Modifies state of viruses
     public void removeVirus(Virus dummy){
         this.viruses.remove(dummy);
         this.removeUnit(dummy);
         dummy.setArea(null);
     }
 
+    // Impure function: Modifies state of antibodies
     public void removeAntibody(Antibody dummy){
         this.antibodies.remove(dummy);
     }
 
-    // 3 level 0 = green light -> all good , 1 = yellow light -> antibodies:viruses = 1:3 , 2 = red light -> area taken
-    public int alertLevel(){ 
+    // Impure function: Output depend on antibodies and viruses size in area (give same input doesn't get the same output)
+    // 3 level: 0 = green light -> all good , 1 = yellow light -> antibodies:viruses = 1:3 , 2 = red light -> area taken
+    public int alertLevel(){
         if(antibodies.isEmpty()){
             return 2;
         }else if(3*this.antibodies.size() <= this.viruses.size()){
@@ -86,112 +98,74 @@ public class Area {
         }
     }
 
-    // change to forEach instead of iterator
-    public void evaluate(){
-        units.forEach(unit -> {
+    // Impure function: Modifies state of units
+    public void evaluate() {
+        Iterator<Unit> unitIterator = units.iterator();
+        while (unitIterator.hasNext()) {
+            Unit unit = unitIterator.next();
             if (unit.isAlive()) {
                 unit.setDidActionCommand(false);
                 unit.evaluate();
-                if (DEBUG) {
-                    System.out.println(name);
-                    System.out.println(unit.getCurrentHealth());
-                }
-            } else {
-                if (unit.getUnitClass().equals("antibody")) {
-                    removeAntibody((Antibody) unit);
-                    units.remove(unit);
+            } else { // if Unit is DEAD
+                if (unit.unitClass.equals("antibody")) {
+                    unitIterator.remove();
                     if (unit.toSpawn) {
                         ((Antibody) unit).virusToSpawn(rebornQueue);
                     } else {
                         unit.setArea(null);
                     }
+                    removeAntibody((Antibody) unit);
                 }
             }
-        });
+        }
     }
 
-    // Using stream instead of for loop
+    // Impure function: Modifies viruses
+    public void snapViruses() {
+        units.removeAll(viruses);
+        viruses.clear();
+    }
+
+    // Pure function
+    // Pure function: Doesn't modify state, only return a value base on inputs
+    // Refactor: Using stream instead of for loop
     public boolean canPlace(double positionX, double positionY){
         return units.stream().noneMatch(u -> u.getPositionX() == positionX && u.getPositionY() == positionY);
     }
 
+    // Impure function: Doesn't modify state, But output depend on value of alertLevel (give same input doesn't get the same output)
     public boolean isTaken(){
         return this.alertLevel() == 2;
     }
 
-
-    public void snapViruses() {
-        units.removeAll(viruses);
-        viruses.clear();
-        if(DEBUG) System.out.println("Virus have gone");
-    }
-
+    // Pure function: Doesn't modify state, only return a value base on inputs
     public int getAlertLevel(){
         return this.alertLevel();
     }
 
+    // Pure function: Doesn't modify state, only return a value base on inputs
     public List<Unit> getUnits() {
-        return units;
+        return this.units;
     }
 
+    // Pure function: Doesn't modify state, only return a value base on inputs
     public List<Virus> getViruses() {
-        return viruses;
+        return this.viruses;
     }
 
+    // Pure function: Doesn't modify state, only return a value base on inputs
     public List<Antibody> getAntibodies() {
-        return antibodies;
+        return this.antibodies;
     }
 
+    // Pure function: Doesn't modify state, only return a value base on inputs
     public String getName() {
-        return name;
+        return this.name;
     }
 
+    // Pure function: Doesn't modify state, only return a value base on inputs
     public double getRadius(){
-        return radius;
+        return this.radius;
     }
 
-//    public static void main(String[] args) {
-//        Area area = new Area("Area1);
-//        Antibody ab = UnitFactory.createAntibody("melee");
-//        ab.setArea(area);
-//        Virus v = UnitFactory.createVirus("melee");
-//        v.setArea(area);
-//        area.addAntibody(ab); area.addVirus(v);
-//    }
-//    Firstly, in the old code, the class Area had several instance variables, including lists for units, viruses, and antibodies. In the new code,
-//    these variables have been replaced with Java Streams, which are more functional and easier to manipulate.
-//
-//    Secondly, the methods in the old code have been refactored to be more functional. For example, the addVirus() and addAntibody() methods in the old code
-//    have been replaced with add() methods in the new code, which use Java Streams to add objects to the appropriate lists.
-//
-//    Thirdly, the old code had an evaluate() method that used a while loop to iterate through all the units in the area. In the new code, this has been replaced
-//    with a single evaluate() method that uses Java Streams to iterate through the units.
-//
-//    Overall, the new code is more concise, easier to read, and more functional than the old code. It makes use of Java Streams, which are a powerful tool for
-//    working with collections of objects, and it uses functional programming techniques to make the code more efficient and easier to understand.
-
-//    Impure functions:
-//
-//    public void addUnit(Unit dummy)
-//    public void addVirus(Virus dummy)
-//    public void addAllVirus(List<Virus> dummy)
-//    public void addAntibody(Antibody dummy)
-//    public void addAllAntibody(List<Antibody> dummy)
-//    public void removeUnit(Unit dummy)
-//    public void removeVirus(Virus dummy)
-//    public void removeAntibody(Antibody dummy)
-//    public void evaluate()
-//    public void snapViruses()
-//    public int alertLevel()
-//    public boolean isTaken()
-
-//    Pure functions:
-//
-//    public boolean canPlace(double positionX, double positionY)
-//    public int getAlertLevel()
-//    public List<Unit> getUnits()
-//    public List<Virus> getViruses()
-//    public List<Antibody> getAntibodies()
-//    public String getName()
-//    public double getRadius()
 }
