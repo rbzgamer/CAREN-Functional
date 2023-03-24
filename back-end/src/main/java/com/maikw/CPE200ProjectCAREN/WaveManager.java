d988dc69a546a07a0fb5a85a11cd66cfb7e42aaa
Autoz <76035467+rbzgamer@users.noreply.github.com>
2023-03-25 05:30:04 +0700

    fixed WaveManager class and add some fields

--- back-end/src/main/java/com/maikw/CPE200ProjectCAREN/WaveManager.java
+++ back-end/src/main/java/com/maikw/CPE200ProjectCAREN/WaveManager.java
@@ -7,38 +7,47 @@ import java.util.Map;
 
 public class WaveManager {
     // Refactor: Remove unnecessary constructor (move setup things to here) and remove some unnecessary variable
-    private Integer maxWaveCount = Config.maxWave;
-    private Integer currentWaveCount = 0;
-    private Integer waveCount = 1;
-    private Integer timeBetweenWave = 20;
-    private Map<String, List<Virus>> allWaves = new HashMap<>();
+    protected Integer maxWaveCount = Config.maxWave;
+    protected Integer currentWaveCount = 0;
+    protected Integer waveCount = 1;
+    protected Integer timeBetweenWave = 20;
+    protected Map<String, List<Virus>> allWaves = new HashMap<>();
+    protected List<Virus> viruses1 = new ArrayList<>();
+    protected List<Virus> viruses2 = new ArrayList<>();
+    protected List<Virus> viruses3 = new ArrayList<>();
 
     // Impure function
     // Impure function: Modifies state of viruses by creating a new list of viruses
-    private List<Virus> createUnitVirus(int areaMelee, int areaRanged, int areaAOE) {
-        List<Virus> viruses = new ArrayList<>();
-        for (int i = 0; i < areaMelee; i++) {
-            viruses.add(UnitFactory.createVirus("melee"));
+    private void createUnitVirus(int areaMelee, int areaRanged, int areaAoe) {
+        for(int i = 0 ; i < areaMelee;i++){
+            viruses1.add(UnitFactory.createVirus( "melee"));
+            viruses2.add(UnitFactory.createVirus( "melee"));
+            viruses3.add(UnitFactory.createVirus( "melee"));
         }
-        for (int i = 0; i < areaRanged; i++) {
-            viruses.add(UnitFactory.createVirus("ranged"));
+        for(int i = 0 ; i < areaRanged ;i++){
+            viruses1.add(UnitFactory.createVirus( "ranged"));
+            viruses2.add(UnitFactory.createVirus( "ranged"));
+            viruses3.add(UnitFactory.createVirus( "ranged"));
         }
-        for (int i = 0; i < areaAOE; i++) {
-            viruses.add(UnitFactory.createVirus("aoe"));
+        for (int i = 0 ; i < areaAoe;i++){
+            viruses1.add(UnitFactory.createVirus( "aoe"));
+            viruses2.add(UnitFactory.createVirus( "aoe"));
+            viruses3.add(UnitFactory.createVirus( "aoe"));
         }
-        return viruses;
+
+        allWaves.put("Wave_"+ waveCount.toString() +"Area_"+0, viruses1);
+        allWaves.put("Wave_"+ waveCount.toString() +"Area_"+1, viruses2);
+        allWaves.put("Wave_"+ waveCount.toString() +"Area_"+2, viruses3);
+        waveCount++;
+        this.viruses1 = new ArrayList<>();
+        this.viruses2 = new ArrayList<>();
+        this.viruses3 = new ArrayList<>();
     }
 
     // Impure function: Modifies state of viruses, waveCount and allWaves
     public void addVirus() {
-        for (int i = 0; i < maxWaveCount; i++) {
-            List<Virus> viruses1 = createUnitVirus(Config.meleeCountPerWave[i], Config.rangedCountPerWave[i], Config.aoeCountPerWave[i]);
-            List<Virus> viruses2 = createUnitVirus(Config.meleeCountPerWave[i], Config.rangedCountPerWave[i], Config.aoeCountPerWave[i]);
-            List<Virus> viruses3 = createUnitVirus(Config.meleeCountPerWave[i], Config.rangedCountPerWave[i], Config.aoeCountPerWave[i]);
-            allWaves.put("Wave_" + waveCount + "_Area_0", viruses1);
-            allWaves.put("Wave_" + waveCount + "_Area_1", viruses2);
-            allWaves.put("Wave_" + waveCount + "_Area_2", viruses3);
-            waveCount++;
+        for (int i = 0 ; i < maxWaveCount ; ++i) {
+            createUnitVirus(Config.meleeCountPerWave[i], Config.rangedCountPerWave[i], Config.aoeCountPerWave[i]);
         }
     }
 
