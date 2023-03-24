f60d86404c62787540fe6a5f129294f207b2e0f1
Autoz <76035467+rbzgamer@users.noreply.github.com>
2023-03-24 15:59:58 +0700

    rewrite Antibody class
    - Separate the impure and pure function
    - move setup things into method and call method instead

--- back-end/src/main/java/com/maikw/CPE200ProjectCAREN/Antibody.java
+++ back-end/src/main/java/com/maikw/CPE200ProjectCAREN/Antibody.java
@@ -5,34 +5,49 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.util.List;
 @JsonIgnoreProperties(value = { "programNode", "variables", "area" })
 public class Antibody extends Unit{
+    // Constructor
     public Antibody(String name, String type, String geneticCode) {
         super(name, type, geneticCode);
         this.unitClass = "antibody";
 
         switch (type) {
             case "melee" -> {
-                this.maxHealth = Config.antibodyMeleeHealth;
-                this.attackDamage = Config.antibodyMeleeDamage;
-                this.attackRange = Config.meleeAttackRange;
-                this.moveSpeed = Config.meleeMoveSpeed;
+                setupMelee();
             }
             case "ranged" -> {
-                this.maxHealth = Config.antibodyRangedHealth;
-                this.attackDamage = Config.antibodyRangedDamage;
-                this.attackRange = Config.rangedAttackRange;
-                this.moveSpeed = Config.rangedMoveSpeed;
+                setupRanged();
             }
             case "aoe" -> {
-                this.maxHealth = Config.antibodyAoeHealth;
-                this.attackDamage = Config.antibodyAoeDamage;
-                this.attackRange = Config.aoeAttackRange;
-                this.aoeRadius = 5;
-                this.moveSpeed = Config.aoeMoveSpeed;
+                setupAoe();
             }
         }
         this.currentHealth = maxHealth;
     }
 
+    // Separate setup things into method
+    private void setupMelee(){
+        this.maxHealth = Config.antibodyMeleeHealth;
+        this.attackDamage = Config.antibodyMeleeDamage;
+        this.attackRange = Config.meleeAttackRange;
+        this.moveSpeed = Config.meleeMoveSpeed;
+    }
+
+    private void setupRanged(){
+        this.maxHealth = Config.antibodyRangedHealth;
+        this.attackDamage = Config.antibodyRangedDamage;
+        this.attackRange = Config.rangedAttackRange;
+        this.moveSpeed = Config.rangedMoveSpeed;
+    }
+
+    private void setupAoe(){
+        this.maxHealth = Config.antibodyAoeHealth;
+        this.attackDamage = Config.antibodyAoeDamage;
+        this.attackRange = Config.aoeAttackRange;
+        this.aoeRadius = 5;
+        this.moveSpeed = Config.aoeMoveSpeed;
+    }
+
+    // Impure function: modifies state of Antibody and virusQueue
     public void virusToSpawn(List<Virus> q){
         Virus reborn = UnitFactory.createVirus(this.getType());
         this.area.removeAntibody(this);
@@ -40,6 +55,11 @@ public class Antibody extends Unit{
         reborn.setPositionX(this.getPositionX());
         reborn.setPositionY(this.getPositionY());
 
+    }
+    
+    // Pure function: doesn't modify state, only return a value base on inputs
+    public String getUnitClass(){
+        return "antibody";
     }
     
 }
